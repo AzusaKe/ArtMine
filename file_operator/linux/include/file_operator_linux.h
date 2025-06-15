1396a01689575f9a623fa6a616a8d2c7ca9ea778
Azusa_Ke <zhuo9_9@outlook.com>
2025-06-15 18:58:28 +0800

    添加必要文件结构4

---
+++ file_operator/linux/include/file_operator_linux.h
@@ -0,0 +1,8 @@
+//
+// Created by Azusa_Ke on 25-6-15.
+//
+
+#ifndef FILE_OPERATOR_LINUX_H
+#define FILE_OPERATOR_LINUX_H
+
+#endif //FILE_OPERATOR_LINUX_H
---
+++ file_operator/windows/file_operator_windows.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Azusa_Ke on 25-6-15.
+//
+
+#include "include/file_operator_windows.h"
