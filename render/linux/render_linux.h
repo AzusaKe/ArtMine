77de23897988329b3dd5d722bf3c591c06dbedcd
Azusa_Ke <zhuo9_9@outlook.com>
2025-06-15 16:26:29 +0800

    添加必要文件结构

---
+++ render/linux/render_linux.h
@@ -0,0 +1,8 @@
+//
+// Created by Azusa_Ke on 25-6-15.
+//
+
+#ifndef RENDER_LINUX_H
+#define RENDER_LINUX_H
+
+#endif //RENDER_LINUX_H
---
+++ render/windows/render_windows.h
@@ -0,0 +1,8 @@
+//
+// Created by Azusa_Ke on 25-6-15.
+//
+
+#ifndef RENDER_WINDOWS_H
+#define RENDER_WINDOWS_H
+
+#endif //RENDER_WINDOWS_H
