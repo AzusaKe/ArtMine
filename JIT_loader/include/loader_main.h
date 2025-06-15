aec58ef2ac27f1890b328a16b5c34849dca60224
Azusa_Ke <zhuo9_9@outlook.com>
2025-06-15 18:31:09 +0800

    添加必要文件结构3

---
+++ JIT_loader/include/loader_main.h
@@ -0,0 +1,8 @@
+//
+// Created by Azusa_Ke on 25-6-15.
+//
+
+#ifndef LOADER_MAIN_H
+#define LOADER_MAIN_H
+
+#endif //LOADER_MAIN_H
---
+++ core/server/world/include/world_generater.h
@@ -0,0 +1,16 @@
+//
+// Created by Azusa_Ke on 25-6-15.
+//
+
+#ifndef WORLD_GENERATER_H
+#define WORLD_GENERATER_H
+
+
+
+class world_generater {
+
+};
+
+
+
+#endif //WORLD_GENERATER_H
