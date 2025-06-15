c97f6b82198b269105b2af3e1cb29a542326e77e
Azusa_Ke <zhuo9_9@outlook.com>
2025-06-16 00:51:52 +0800

    添加必要文件

--- element/block/dirt.cpp
+++ element/block/dirt.cpp
@@ -4,13 +4,20 @@
 
 #include "dirt.h"
 
+#include <iostream>
+
+const int dirt::ID;
+
 dirt dirt::INSTANCE;
 
 namespace {
-    bool registered = [] {
+    // 使用函数局部静态变量来确保正确的初始化顺序
+    bool register_dirt() {
         BLOCK_REGISTRY[dirt::ID] = &dirt::INSTANCE;
         return true;
-    };
+    }
+
+    bool registered = register_dirt();
 }
 
 dirt::dirt() {
@@ -20,4 +27,8 @@ dirt::dirt() {
     Block_Data.block_light_level = 0;
     Block_Data.block_direction = Block_Direction::UP;
     Block_Data.block_r_level = R_Level::R_LEVEL_0;
+}
+
+void dirt::on_block_placed() {
+    std::cout << "Dirt block placed!" << std::endl;
 }
