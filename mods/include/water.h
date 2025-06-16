7b0218ecdb1534ec314d60b511d31c0a18d9f7f2
Azusa_Ke <zhuo9_9@outlook.com>
2025-06-16 17:52:35 +0800

    尝试加入dll动态加载

---
+++ mods/include/water.h
@@ -0,0 +1,17 @@
+//
+// Created by Azusa_Ke on 25-6-16.
+//
+
+#ifndef WATER_H
+#define WATER_H
+
+#include "block.h"
+
+class __declspec(dllexport) water : public block {
+    public:
+    water();
+    void on_block_placed() override;
+    static const int ID = 3;
+};
+
+#endif //WATER_H
---
+++ mods/water.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Azusa_Ke on 25-6-16.
+//
+
+#include "include/water.h"
+
+#include <iostream>
+
+water::water() {
+    Block_Data.block_id = "minecraft:water";
+    Block_Data.block_type = Block_Type::LIQUID;
+    Block_Data.block_hardness = -1;
+    Block_Data.block_light_level = 0;
+    Block_Data.block_direction = Block_Direction::UP; // 水通常从上方流动
+    Block_Data.block_r_level = R_Level::R_LEVEL_0;
+}
+
+void water::on_block_placed() {
+    // 水块被放置时的逻辑
+    std::cout << "Water block placed!" << std::endl;
+}
+
+static water water_block;
+
+extern "C" __declspec(dllexport) void init_water_mod() {
+    // 初始化水模块
+    register_block(water_block.ID, &water_block);
+}
